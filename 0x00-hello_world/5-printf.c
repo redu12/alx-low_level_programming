5d99b856f7c4825a2dc7cec95df6aa764d88b5c5
root <redietasress@gmail.com>
2023-02-16 09:47:30 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 
 /**
  * main - main block
- *
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
